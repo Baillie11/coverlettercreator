44d568cca9b019b7540071e6b2a377ed86e02f81
Baillie11 <andrewgbaillie@hotmail.com>
2021-09-03 12:35:32 +1000

    html card file added, but css not working yet

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+//Imports
 const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.static('public'))
+app.use('/css/main.css', express.static(__dirname + 'public/css/main.css'))
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+    console.log(__dirname)
 })
 
+//Listen on port 3000
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
